03a2033ea315a6fe93a6dc3294922acdf9d0c29b
KittiKat3002 <58003950+KittiKat3002@users.noreply.github.com>
2020-01-06 12:45:56 -0500

    You can see it now
    Finally got graphics to work

--- src/game.py
+++ src/game.py
@@ -3,9 +3,29 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((400, 400))
+display_height = 400
+display_width = 400
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+greeen = (100, 200, 0)
+
+screen = pygame.display.set_mode((display_width, display_height))
+
+char = pygame.image.load("src/AAAH.jpg")
+
+def charpl(x, y):
+    screen.blit(char, (x,y))
+
+xp =  (display_width * 0.45)
+yp = (display_height * 0.6)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+    screen.fill(greeen)
+
+    charpl(xp, yp)
+
+    pygame.display.flip()
