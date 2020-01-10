8511a3e2a54818009a89133c4b3c9abae12d9aa6
KittiKat3002 <58003950+KittiKat3002@users.noreply.github.com>
2020-01-10 13:03:25 -0500

    More stuff
    Finished the player class and added platform class, started level and enemy classes

--- src/game.py
+++ src/game.py
@@ -5,6 +5,8 @@ import pygame
 
 pygame.init()
 
+Fail = False
+
 display_height = 600
 display_width = 800
 
@@ -34,6 +36,50 @@ class Player(Pygame.sprite.Sprite):
 
         block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
         for block in block_hit_list:
+            if self.change_x > 0:
+                self.rect.right = block.rect.left
+             if self.change_x < 0:
+                self.rect.left = block.rect.right
+        
+        block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        for block in block_hit_list:
+            if self.change_y > 0:
+                self.rect.bottom = block.rect.top
+             if self.change_y < 0:
+                self.rect.top = block.rect.bottom
+
+            self.change_y = 0
+
+    def calc_grav(self):
+        if self.change_y == 0:
+            self.change_y = 1
+        else:
+            self.change_y = .35
+        if self.rect.y >= display_height - self.rect.height and self.change_y >= 0:
+            Fail = True
+
+    def jump(self):
+        self.change_y = -10
+    def left(self):
+        self.change_x = -6
+    def right(self):
+        self.change_x = 6
+    def stop(self):
+        self.change_x = 0
+
+class Platform(pygame.sprite.Sprite):
+
+    def __init__(self, width, height, color):
+        super().__init__()
+        self.image = pygame.Surface([width. height)])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+
+class Level(object):
+    def __init__(self, player):
+
+class enemy(pygame.sprite.Sprite):
+
 
 xp =  (display_width * 0.45)
 yp = (display_height * 0.35)
