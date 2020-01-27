ad23853bcab73702360dee89f5488903e18e4b4d
KittiKat3002 <58003950+KittiKat3002@users.noreply.github.com>
2020-01-27 15:08:13 -0800

    small edits
    I don't know why it still isn't working it's giving me errors that don't make sense

--- src/game.py
+++ src/game.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 
+pygame.font.init
+
 Screen_width = 800
 Screen_height = 800
 
@@ -8,22 +10,24 @@ White = (255, 255, 255)
 Black = (0, 0, 0)
 Grade = 5
 
-class Player (pygame.sprite.Sprite):
+Font = pygame.font.Font("timesnewroman.ttf", 12)
+
+class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        self.image = ("img/mouse.jpg")
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("src/mouse.jpg")
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.platforms = False
         self.change_y = 0
         self.change_x = 0
 
     def update(self):
         self.rect.x += self.change_x
         self.rect.y += self.change_y
-        self.level = None
+        self.level = []
     
-        Hit_list = pygame.sprite.spritecollide(self, self.level.platforms, False)
+        Hit_list = pygame.sprite.spritecollide(self, self.level, False)
         for P in Hit_list:
             if self.change_x > 0:
                 self.rect.right = P.rect.left
@@ -42,7 +46,7 @@ class Player (pygame.sprite.Sprite):
         else:
             self.change.y += .5
 
-        if self.rect.y >= screen_height - self.rect.height and self.change_y >= 0:
+        if self.rect.y >= Screen_height - self.rect.height and self.change_y >= 0:
             Grade -= 1
             self.change.y = 0
             self.change.x =0
@@ -51,7 +55,7 @@ class Player (pygame.sprite.Sprite):
 
     def ymove(self):
         self.rect.y += 2
-        Hit_list = pygame.sprite.spritecollide(self, self.platforms, False)
+        Hit_list = pygame.sprite.spritecollide(self, self.level, False)
         self.rect.y -= 2
 
         if len(Hit_list) > 0:
@@ -67,28 +71,30 @@ class Player (pygame.sprite.Sprite):
         self.change.x = 0
 
 class Platform(pygame.sprite.Sprite):
-    def __init__(self, wide, tall):
+    def __init__(self, wide, tall, x, y):
+        pygame.sprite.Sprite.__init__(self)
         super().__init__()
         self.image = pygame.Surface([wide, tall])
         self.image.fill(White)
-        self.rect = self.image.get_rect
+        self.rect1 = self.image.get_rect()
+        self.rect1.x = x
+        self.rect1.y = y
 
 class layout(object):
     def __init__(self, player):
         self.platform_list = pygame.sprite.Group
         self.player = player
-        self.background = ("img/background.jpg")
+        self.background = pygame.image.load("src/background.jpg")
 
-    level = [[210, 70, 500, 500],
-        [210, 70, 200, 400],
-        [210, 70, 600, 300],
-        ]
+        level = [[210, 70, 500, 500],
+            [210, 70, 200, 400],
+            [210, 70, 600, 300],
+            ]
 
-    for platform in level:
-        P = Platform(platform[0], platform[1])
-        P.rect.center = (platform[2], platform[3])
-        P.player = self.player
-        self.platform_list.add(p)
+        for platform in level:
+            P = Platform(platform[0], platform[1], platform[2], platform[3])
+            P.player = self.player
+            self.platform_list.add(P)
 
     def update(self):
         self.platform_list.update
@@ -99,7 +105,8 @@ class layout(object):
 
 class finalpaper(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        self.image = ("img/paper.jpg")
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("src/paper.jpeg")
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -109,37 +116,33 @@ def maingame():
     pygame.font.init()
     screen_size = [Screen_width, Screen_height]
     Screen = pygame.display.set_mode(screen_size)
-    Player = Player()
-    Paper = finalpaper()
+    player1 = Player(340, 100)
+    Paper = finalpaper(200, 100)
     Grademessage = "Sample"
 
-    if Grade == 5:
-	    Grademessage = "If turned in now you will get an A"
-    elif Grade == 4:
-	    Grademessage = "If turned in now you will get a B"
-    elif Grade == 3:
-        Grademessage = "If turned in now you will get a C"
-    elif Grade == 2:
-        Grademessage = "If turned in now you will get a D"
-    elif Grade == 1:
-        Grademessage = "If turned in now you will get an F"
-    else:
-        Grademessage = "You couldn’t turn it in and got a 0"
+    def grading():
+        if Grade == 5:
+	        Grademessage = "If turned in now you will get an A"
+        elif Grade == 4:
+	        Grademessage = "If turned in now you will get a B"
+        elif Grade == 3:
+            Grademessage = "If turned in now you will get a C"
+        elif Grade == 2:
+            Grademessage = "If turned in now you will get a D"
+        elif Grade == 1:
+            Grademessage = "If turned in now you will get an F"
+        else:
+            Grademessage = "You couldn’t turn it in and got a 0"
 
-    Font = pygame.font.Font("timesnewroman.ttf", 12)
-    Text = Font.render(Grademessage, True, Black, White)
-    Textrect = text.get_rect
-    Textrect.center = (Screen_width/2, Screen_height/3)
+        Text = Font.render(Grademessage, True, Black, White)
+        Textrect = Text.get_rect
+        Textrect.center = (Screen_width/2, Screen_height/3)
+        Screen.blit(Text, Textrect)
 
     Sprites_list = pygame.sprite.Group()
-    Player.level = Layout()
+    player1.level = layout(player1)
 
-    Player.rect.x = 340
-    Player.rect.y = 100
-    Paper.rect.x = 240
-    Paper.rect.y = 100
-
-    Sprites_list.add(Player)
+    Sprites_list.add(player1)
     Sprites_list.add(Paper)
 
     Won = False
@@ -149,51 +152,51 @@ def maingame():
             if event.type == pygame.QUIT:
                 sys.exit
 
-            if event.type == pygame.KEY_DOWN:
+            if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_Left:
-                    Player.Lmove()
+                    player1.Lmove()
                 if event.key == pygame.K_Right:
-                    Player.Rmove()
+                    player1.Rmove()
                 if event.key == pygame.K_Up:
-                    Player.ymove()
+                    player1.ymove()
 
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_LEFT and player.change_x < 0:
-                        Player.stop()
-                    if event.key == pygame.K_RIGHT and player.change_x > 0:
-                        Player.stop()
+                    if event.key == pygame.K_LEFT and player1.change_x < 0:
+                        player1.stop()
+                    if event.key == pygame.K_RIGHT and player1.change_x > 0:
+                        player1.stop()
 
-        if Player.rect.colliderect(Paper.rect):
+        if player1.rect.colliderect(Paper.rect):
             Won = True
 
-        if Grade == 0:
+        if Grade < 0:
             Won = True
 		
+        if player1.rect.x > Screen_width:
+            player1.rect.right = Screen_width
+        if player1.rect.x < 0:
+            player1.rect.left = 0
+
         Sprites_list.update()
-        Layout.update()
-        Screen.blit(Text, Textrect)
+        layout.update()
 
-        if player.rect.x > Screen_width:
-            Player.rect.right = Screen_width
-        if player.rect.x < 0:
-            Player.rect.left = 0
-
-        Layout.draw(Screen)
+        layout.draw(Screen)
         Sprites_list.draw(Screen)
+        grading()
 
         clock.tick(60)
         pygame.display.flip
 
-    if Grade > 5:
-        Screen.fill(white)
+    if Grade > 0:
+        Screen.fill(White)
         Text = Font.render("You won! You handed the paper in on time!", True, black, white)
-        Textrect = text.get_rect
-        Textrect.center = (screen_width/2, screen_height/3)
+        Textrect = Text.get_rect
+        Textrect.center = (Screen_width/2, Screen_height/3)
 
     else:
         Screen.fill(White)
-        Text = Font.render("You lost. You couldn’t get the paper to print. You shouldn’t have waited until the last day huh", True, black, white)
-        Textrect = text.get_rect
-        Textrect.center = (screen_width/2, screen_height/3)
+        Text = Font.render("You lost. You couldn’t get the paper to print.", True, black, white)
+        Textrect = Text.get_rect
+        Textrect.center = (Screen_width/2, Screen_height/3)
 
 maingame()
