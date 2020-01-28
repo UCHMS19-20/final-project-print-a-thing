dd5ef5302983fe9f562bd7d3a87f0d103776d2e6
KittiKat3002 <58003950+KittiKat3002@users.noreply.github.com>
2020-01-27 18:37:38 -0800

    More things??
    It still isn't fully working because of course it isn't why would it be????!!!!

--- src/game.py
+++ src/game.py
@@ -1,16 +1,20 @@
 import sys
 import pygame
 
-pygame.font.init
+pygame.init()
 
 Screen_width = 800
 Screen_height = 800
+pygame.font.init()
+screen_size = [Screen_width, Screen_height]
+Screen = pygame.display.set_mode(screen_size)
 
 White = (255, 255, 255)
 Black = (0, 0, 0)
 Grade = 5
 
-Font = pygame.font.Font("timesnewroman.ttf", 12)
+Font = pygame.font.SysFont("comicsansms", 12)
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -25,20 +29,20 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         self.rect.x += self.change_x
         self.rect.y += self.change_y
-        self.level = []
+        #self.levl = None
     
-        Hit_list = pygame.sprite.spritecollide(self, self.level, False)
-        for P in Hit_list:
-            if self.change_x > 0:
-                self.rect.right = P.rect.left
-            if self.change_x < 0:
-                self.rect.left = P.rect.right
-        for P in Hit_list:
-            if self.change_y > 0:
-                self.rect.bottom = P.rect.top
-            if self.change_y < 0:
-                self.rect.top = P.rect.bottom
-                self.change_y = 0
+      #  Hit_list = pygame.sprite.spritecollide(self, self.levl.platform_list, False)
+      #  for P in Hit_list:
+       #     if self.change_x > 0:
+       #         self.rect.right = P.rect.left
+        #    if self.change_x < 0:
+         #       self.rect.left = P.rect.right
+    #    for P in Hit_list:
+     #       if self.change_y > 0:
+      #          self.rect.bottom = P.rect.top
+       #     if self.change_y < 0:
+        #        self.rect.top = P.rect.bottom
+         #       self.change_y = 0
 
     def fall(self):
         if self.change.y == 0:
@@ -55,7 +59,7 @@ class Player(pygame.sprite.Sprite):
 
     def ymove(self):
         self.rect.y += 2
-        Hit_list = pygame.sprite.spritecollide(self, self.level, False)
+        Hit_list = pygame.sprite.spritecollide(self, False)
         self.rect.y -= 2
 
         if len(Hit_list) > 0:
@@ -80,27 +84,31 @@ class Platform(pygame.sprite.Sprite):
         self.rect1.x = x
         self.rect1.y = y
 
-class layout(object):
+class layoutmaster(object):
     def __init__(self, player):
         self.platform_list = pygame.sprite.Group
         self.player = player
         self.background = pygame.image.load("src/background.jpg")
 
+    def update(self):
+        self.platform_list.update
+
+class Layout1(layoutmaster):
+    def __init__(self, player):
+        layoutmaster.__init__(self, player)
         level = [[210, 70, 500, 500],
             [210, 70, 200, 400],
             [210, 70, 600, 300],
             ]
+        self.platform_list = []
 
         for platform in level:
             P = Platform(platform[0], platform[1], platform[2], platform[3])
             P.player = self.player
             self.platform_list.add(P)
-
-    def update(self):
-        self.platform_list.update
-
+    
     def draw(self, Screen):
-        self.background.draw(Screen)
+        Screen.fill(White)
         self.platform_list.draw(Screen)
 
 class finalpaper(pygame.sprite.Sprite):
@@ -112,10 +120,6 @@ class finalpaper(pygame.sprite.Sprite):
         self.rect.y = y
 
 def maingame():
-    pygame.init()
-    pygame.font.init()
-    screen_size = [Screen_width, Screen_height]
-    Screen = pygame.display.set_mode(screen_size)
     player1 = Player(340, 100)
     Paper = finalpaper(200, 100)
     Grademessage = "Sample"
@@ -140,7 +144,6 @@ def maingame():
         Screen.blit(Text, Textrect)
 
     Sprites_list = pygame.sprite.Group()
-    player1.level = layout(player1)
 
     Sprites_list.add(player1)
     Sprites_list.add(Paper)
@@ -153,11 +156,11 @@ def maingame():
                 sys.exit
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_Left:
+                if event.key == pygame.K_LEFT:
                     player1.Lmove()
-                if event.key == pygame.K_Right:
+                if event.key == pygame.K_RIGHT:
                     player1.Rmove()
-                if event.key == pygame.K_Up:
+                if event.key == pygame.K_UP:
                     player1.ymove()
 
                 if event.type == pygame.KEYUP:
@@ -169,6 +172,17 @@ def maingame():
         if player1.rect.colliderect(Paper.rect):
             Won = True
 
+        if player1.rect.collidelist(Layout1.platform_list):
+            if player1.change_x > 0:
+                player1.rect.right = P.rect.left
+            if player1.change_x < 0:
+                player1.rect.left = P.rect.right
+            if player1.change_y > 0:
+                player1.rect.bottom = P.rect.top
+            if player1.change_y < 0:
+                player1.rect.top = P.rect.bottom
+                player1.change_y = 0
+
         if Grade < 0:
             Won = True
 		
@@ -177,25 +191,24 @@ def maingame():
         if player1.rect.x < 0:
             player1.rect.left = 0
 
-        Sprites_list.update()
-        layout.update()
+        Layout1.update()
 
-        layout.draw(Screen)
+        Layout1.draw(Screen)
         Sprites_list.draw(Screen)
         grading()
 
-        clock.tick(60)
+        pygame.time.Clock.tick(60)
         pygame.display.flip
 
     if Grade > 0:
         Screen.fill(White)
-        Text = Font.render("You won! You handed the paper in on time!", True, black, white)
+        Text = Font.render("You won! You handed the paper in on time!", True, Black, White)
         Textrect = Text.get_rect
         Textrect.center = (Screen_width/2, Screen_height/3)
 
     else:
         Screen.fill(White)
-        Text = Font.render("You lost. You couldn’t get the paper to print.", True, black, white)
+        Text = Font.render("You lost. You couldn’t get the paper to print.", True, Black, White)
         Textrect = Text.get_rect
         Textrect.center = (Screen_width/2, Screen_height/3)
 
