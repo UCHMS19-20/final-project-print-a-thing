d51eafec1e36deefba5f58df2db64887d52e42f3
KittiKat3002 <58003950+KittiKat3002@users.noreply.github.com>
2020-01-30 17:56:20 -0800

    Code finished
    The cod is functional, i have win and lose conditions and a win screen. I may add more features later but this works

--- src/game.py
+++ src/game.py
@@ -1,5 +1,8 @@
 import pygame
- 
+import sys
+import random
+pygame.init()
+pygame.font.init()
 # Global constants
  
 # Colors
@@ -12,12 +15,15 @@ BLUE = (0, 0, 255)
 # Screen dimensions
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
- 
+
+Grade = 5
+font = pygame.font.Font("freesansbold.ttf", 22)
+Win = False
+Lose = False
  
 class Player(pygame.sprite.Sprite):
-    """ This class represents the bar at the bottom that the player
-        controls. """
- 
+    """ This class represents mouse cursor that the player controls """
+    global Grade
     # -- Methods
     def __init__(self):
         """ Constructor function """
@@ -25,8 +31,7 @@ class Player(pygame.sprite.Sprite):
         # Call the parent's constructor
         super().__init__()
  
-        # Create an image of the block, and fill it with a color.
-        # This could also be an image loaded from the disk.
+        # Load image of the mouse
         self.image = pygame.image.load("src/mouse.jpg")
  
         # Set a referance to the image rect.
@@ -41,13 +46,18 @@ class Player(pygame.sprite.Sprite):
  
     def update(self):
         """ Move the player. """
-        # Gravity
+        # Globls needed for this bit
+        global font
+        global Grade
+        global Text
+        global Textrect
+        #need gravity for y movement
         self.calc_grav()
  
         # Move left/right
         self.rect.x += self.change_x
  
-        # See if we hit anything
+        # See if we hit any platforms
         block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
         for block in block_hit_list:
             # If we are moving right,
@@ -73,9 +83,30 @@ class Player(pygame.sprite.Sprite):
  
             # Stop our vertical movement
             self.change_y = 0
+        if Grade == 5:
+            Text = font.render("Go for it, get that A!", True, BLACK, WHITE)
+            Textrect = Text.get_rect()
+            Textrect.center = (400, 100)
+        elif Grade == 4:
+            Text = font.render("Keep going, a B's pretty good!", True, BLACK, WHITE)
+            Textrect = Text.get_rect()
+            Textrect.center = (400, 100)
+        elif Grade == 3:
+            Text = font.render("Hurry up! A C could be worse!", True, BLACK, WHITE)
+            Textrect = Text.get_rect()
+            Textrect.center = (400, 100)
+        elif Grade == 2:
+            Text = font.render("Oof, a D. At least you aren't failing", True, BLACK, WHITE)
+            Textrect = Text.get_rect()
+            Textrect.center = (400, 100)
+        elif Grade == 1:
+            Text = font.render("Hurry up! Hurry up! You may have an F but it's better than a 0!", True, BLACK, WHITE)
+            Textrect = Text.get_rect()
+            Textrect.center = (400, 100)
  
     def calc_grav(self):
         """ Calculate effect of gravity. """
+        global Grade
         if self.change_y == 0:
             self.change_y = 1
         else:
@@ -85,6 +116,11 @@ class Player(pygame.sprite.Sprite):
         if self.rect.y >= SCREEN_HEIGHT - self.rect.height and self.change_y >= 0:
             self.change_y = 0
             self.rect.y = SCREEN_HEIGHT - self.rect.height
+            Grade -= 1
+            print(Grade)
+            self.rect.y = 300
+            self.rect.x = 50
+
  
     def jump(self):
         """ Called when user hits 'jump' button. """
@@ -127,7 +163,19 @@ class Platform(pygame.sprite.Sprite):
         self.image.fill(WHITE)
  
         self.rect = self.image.get_rect()
+
+class Paper(pygame.sprite.Sprite):
+    """ Paper that is the objective """
  
+    def __init__(self, x, y):
+        """ Constructor. Needs location for different layouts """
+        super().__init__()
+ 
+        self.image = pygame.image.load("src/paper.jpeg")
+ 
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
  
 class Level(object):
     """ This is a generic super-class used to define a level.
@@ -135,8 +183,7 @@ class Level(object):
         info. """
  
     def __init__(self, player):
-        """ Constructor. Pass in a handle to player. Needed for when moving platforms
-            collide with the player. """
+        """ Constructor. Pass in a handle to player. """
         self.platform_list = pygame.sprite.Group()
         self.enemy_list = pygame.sprite.Group()
         self.player = player
@@ -148,7 +195,6 @@ class Level(object):
     def update(self):
         """ Update everything in this level."""
         self.platform_list.update()
-        self.enemy_list.update()
  
     def draw(self, screen):
         """ Draw everything on this level. """
@@ -158,7 +204,6 @@ class Level(object):
  
         # Draw all the sprite lists that we have
         self.platform_list.draw(screen)
-        self.enemy_list.draw(screen)
  
  
 # Create platforms for the level
@@ -174,7 +219,7 @@ class Level_01(Level):
         # Array with width, height, x, and y of platform
         level = [[150, 50, 300, 270],
                  [150, 50, 60, 400],
-                 [150, 50, 600, 100],
+                 [150, 50, 600, 130],
                  ]
  
         # Go through the array above and add platforms
@@ -189,7 +234,8 @@ class Level_01(Level):
 def main():
     """ Main Program """
     pygame.init()
- 
+    global Win
+    global Lose
     # Set the height and width of the screen
     size = [SCREEN_WIDTH, SCREEN_HEIGHT]
     screen = pygame.display.set_mode(size)
@@ -198,6 +244,7 @@ def main():
  
     # Create the player
     player = Player()
+    papel = Paper(random.randint(200, 500), 90)
  
     # Create all the levels
     level_list = []
@@ -239,36 +286,53 @@ def main():
                     player.stop()
                 if event.key == pygame.K_RIGHT and player.change_x > 0:
                     player.stop()
- 
+            fin = pygame.sprite.collide_rect(papel, player)
+            if fin > 0:
+                Win = True
         # Update the player.
+        print(Grade)
         active_sprite_list.update()
- 
-        # Update items in the level
+
+        # Update platforms in the level
         current_level.update()
- 
-        # If the player gets near the right side, shift the world left (-x)
+
+        # If the player gets near the right side, stop them
         if player.rect.right > SCREEN_WIDTH:
             player.rect.right = SCREEN_WIDTH
  
-        # If the player gets near the left side, shift the world right (+x)
+        # If the player gets near the left side, stop them
         if player.rect.left < 0:
             player.rect.left = 0
  
-        # ALL CODE TO DRAW SHOULD GO BELOW THIS COMMENT
+        # draw all the objects on screen
         current_level.draw(screen)
         active_sprite_list.draw(screen)
- 
-        # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
+        screen.blit(papel.image, (papel.rect.x, papel.rect.y))
+        screen.blit(Text, Textrect)
+
+        if Grade == 0:
+            Lose = True
+        
  
         # Limit to 60 frames per second
         clock.tick(60)
  
-        # Go ahead and update the screen with what we've drawn.
+        # update screen
         pygame.display.flip()
- 
-    # Be IDLE friendly. If you forget this line, the program will 'hang'
-    # on exit.
-    pygame.quit()
+
+        if Win == True:
+            done = True
+        if Lose == True:
+            done = True
+    
+    while Win == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit
+        screen.fill(WHITE)
+        pic = pygame.image.load("src/Success.jpg")
+        screen.blit(pic, (0, 50))
+        pygame.display.flip()
  
 if __name__ == "__main__":
     main()
